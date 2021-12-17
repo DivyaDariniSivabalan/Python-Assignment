78c69f6032499381d1ec255ecbd5e580b1019c5a
DivyaDariniSivabalan <95759871+DivyaDariniSivabalan@users.noreply.github.com>
2021-12-17 13:58:29 +0530

    Add files via upload

---
+++ live session assignment 1.py
@@ -0,0 +1,14 @@
+print("1.ADDITION \t 2.SUBTRACTION")
+a=int(input("FIRST NUMBER:"))
+b=int(input("SECOND NUMBER:"))
+c=int(input("CHOICE NO:"))
+
+if(c==1):
+    print("ADDITION OF THE NUMBERS:",a+b)
+
+elif(c==2):
+    print("SUBTRACTION OF THE NUMBERS:",a-b)
+
+else:
+    print("CHOOSE A VALID OPTION")
+    
