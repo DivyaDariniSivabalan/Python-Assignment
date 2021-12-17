e6e334b9192bf0cfc997702b1647a6180f27c4f5
DivyaDariniSivabalan <95759871+DivyaDariniSivabalan@users.noreply.github.com>
2021-12-17 14:33:42 +0530

    Add files via upload

---
+++ live assignment 2.py
@@ -0,0 +1,7 @@
+def message(a,b,c):
+    print(a+" "+b+" "+c+" ")
+message("divya","darsini","sivabalan")
+
+def message(a,b,c,d):
+    print(a+" "+b+" "+c+" "+d+" ")
+message("shinchan","himawari","kazaama","nenechan")
