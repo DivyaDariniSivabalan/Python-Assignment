2cfade3c83b0e2c94715f49ba77211b12bbbf9fc
DivyaDariniSivabalan <95759871+DivyaDariniSivabalan@users.noreply.github.com>
2021-12-17 19:39:02 +0530

    Add files via upload

---
+++ live assignment 3.py
@@ -0,0 +1,18 @@
+#using AND #
+x = 5
+print(x > 3 and x < 10)
+#using OR #
+a = 1
+b = 2
+if a or b:
+    print("Atleast one number has boolean value as True")
+else:
+    print("All the numbers have boolean value as False")
+#using NOT #
+a =  12
+if not (a%2 == 0 or a%6 == 0):
+	print("12 is not divisible by either 2 or 6")
+else:
+	print("12 is divisible by either 2 or 6")
+
+    
